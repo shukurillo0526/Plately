d700ecec0c8870ff3bde98788ae4835b0ecf7801
MAMARASULOV SHUKURILLOKH SHUKHRATBEK U <shukurillomamarasulovex@gmail.com>
2026-06-21 16:24:53 +0500

    Update static version constants to 0.1.4+4 to match pubspec.yaml

--- frontend/lib/core/constants/app_info.dart
+++ frontend/lib/core/constants/app_info.dart
@@ -1,6 +1,6 @@
 class AppInfo {
-  static const String version = '0.0.2';
-  static const String buildNumber = '1';
+  static const String version = '0.1.4';
+  static const String buildNumber = '4';
   static const String appName = 'Plately';
 
   static String get formattedVersion => 'v$version';
